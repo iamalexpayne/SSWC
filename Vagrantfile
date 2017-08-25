139284be0301b003372a74d29b52dff2e88b99c7
Dayron Edmundo Acosta <dayroned@gmail.com>
2017-08-25 17:33:39 -0400

    Updated Production Settings

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.box_version = "20170815.1.0"
   config.vm.network "forwarded_port", guest: 8000, host: 8000
+  config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.provision "shell", inline: $shell
 end
 
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ import json
 from django.core.exceptions import ImproperlyConfigured
 
 # JSON-based secrets module
-with open('secrets.json') as f:
+with open(os.path.join(BASE_DIR, 'secrets.json')) as f:
 	secrets = json.loads(f.read())
 
 def get_secret(setting, secrets=secrets):
@@ -16,6 +16,17 @@ def get_secret(setting, secrets=secrets):
 		error_msg = 'Set the {0} environment variable'.format(setting)
 		raise ImproperlyConfigured(error_msg)
 
+ALLOWED_HOSTS = ['*']
+
 SECRET_KEY = get_secret('SECRET_KEY')
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
